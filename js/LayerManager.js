292892ea6d3e0073c2df2a7de4516cab686fd461
Mattia Fusaro <mattiafusaro8@gmail.com>
2016-06-13 18:38:27 +0200

    icone e lista mappe

--- js/LayerManager.js
+++ js/LayerManager.js
@@ -1,6 +1,6 @@
 define(['lodash'], function(_) {
 	"use strict";
-	
+
 	// Handle things related to the overlays
 	function LayerManager(dataService, mapManager, defaultEnabledLayer) {
 		var self = this;
@@ -10,21 +10,22 @@ define(['lodash'], function(_) {
 			selectedPoly = null,
 			selectedData = null,
 			activeLandmarksObj = {}; // keyed by feature id
-					
+
 		/* Create a menu item for each layer
 		*/
 		this.initMenu = function () {
 			dataService.get('layers', function (snapshot) {
 				var data = snapshot.val();
+
 				$(document).ready(function() {
 					var newOption = '<li role="presentation"><a role="menuitem" id="'+data.id+'-layer" href="#">'+data.name+'</a></li>';
-			
+
 					if (data.parent) {
 						var strippedParentName = data.parent.replace(/\s/g, '');
 						if ($('#'+strippedParentName+'-menu').length === 0) {
 							var newGroup = ' <li class="dropdown-submenu"><a href="#">'+data.parent+'</a><ul id="'+strippedParentName+'-menu" class="dropdown-menu"></ul></li>';
 							$('#new-layer-parent-other').before('<option value="'+data.parent+'">'+data.parent+'</option>');
-			
+
 							if ($('#new-layer-menu').length > 0) {
 								$('#new-layer-menu').before(newGroup);
 							} else {
@@ -39,12 +40,12 @@ define(['lodash'], function(_) {
 							$('.layers-menu').append(newOption);
 						}
 					}
-					
+
 					$('.layers-menu').removeClass('loading');
-			
+
 					$('#' + data.id + '-layer').click(self.toggleLayer.bind(this, data.id, data.color));
 					$('.layers-select').append('<option value="'+data.id+'">'+data.name+'</option>');
-					
+
 					if (defaultEnabledLayer && data.id == defaultEnabledLayer) {
 					  console.log("Imposto colore Layer ", data.id, data.color);
 					  layerColors[data.id] = data.color;
@@ -53,19 +54,19 @@ define(['lodash'], function(_) {
 				});
 			});
 		};
-		
+
 		this.mostRecentlyEnabled = function() {
 			return enabledLayers.length ? enabledLayers[enabledLayers.length-1] : null;
 		};
-		
+
 		this.selectedData = function() {
 			return selectedData;
 		};
-		
+
 		this.selectedPoly = function() {
 			return selectedPoly;
 		};
-		
+
 		/* Add a new layer to the map.
 		 * Updates the database and the layer menu
 		 */
@@ -87,8 +88,8 @@ define(['lodash'], function(_) {
 
 			// TODO generalize for any account
 			dataService.fb.child('layers').push({
-				name: name, 
-				id: id, 
+				name: name,
+				id: id,
 				color: color,
 				parent: parent,
 				createdBy: dataService.auth.getUser().uid
@@ -107,7 +108,7 @@ define(['lodash'], function(_) {
 		this.clonePoly = function () {
 			var newData = $.extend(true, {}, selectedData); // clone the data
 			newData.properties.type = $('#clone-layer-type').val();
-			
+
 			// dataService.fb.child('vpc/features').push(newData);
 			var newRef = dataService.fb.child('features').push();
 			console.log("Nuova Chiave", newRef.key(), newData);
@@ -119,24 +120,24 @@ define(['lodash'], function(_) {
 			$('#clone-layer').modal('hide');
 			mapManager.map.closePopup();
 		};
-		
+
 		/* Shows the edit modal
 		 */
 		this.editModal = function () {
 		  console.log("selectedData", selectedData);
-		  
+
 		  $('#edit-feature-id'  ).html(selectedData.id            );
 		  $('#edit-feature-name').val(selectedData.properties.name);
 		  $('#edit-feature-type').val(selectedData.properties.type);
 		  $('#edit-feature-link').val(selectedData.properties.link);
-		  
+
 			$('#edit-layer').modal('show');
-			
+
 			// var name = $('#new-layer-name').val();
 			// var id = $('#new-layer-id').val();
 			// var color = $('#new-layer-color').val();
 			// var parent = $('#new-layer-parent').val();
-      // 
+      //
 			// if (parent === "Other") {
 			// 	parent = $('#new-layer-new-parent').val();
 			// }
@@ -167,22 +168,22 @@ define(['lodash'], function(_) {
 		    mapManager.map.closePopup();
 		  });
 		};
-		
+
 		this.deletePoly = function(){
 		  if (!confirm("DELETE feature " + selectedData.id)) return;
-		  
+
 		  var cur_type = selectedData.properties.type;
-		  
+
 		  dataService.deleteItem(selectedData);
-  		
+
   		console.log("dd", dataService.currentMap(), selectedData.id);
 		  dataService.fb.child('features').child(selectedData.id).remove();
 		  dataService.fb.child('geometries').child(dataService.currentMap()).child(selectedData.id).remove();
-		  
+
 		  myDisableLayer(cur_type);
   		myEnableLayer(cur_type);
 		}
-		
+
 		/* Turns a overlay layer on or off.
 		 * When layers are enabled, only the enalbed layers' features
 		 * appear in the search.
@@ -197,17 +198,17 @@ define(['lodash'], function(_) {
 
 			self.updateAutocomplete();
 		};
-		
+
 		this.enableLayer = function(type, color, selectedFeatureId) {
 			if (color) {
 				layerColors[type] = color;
 			} else {
 				color = layerColors[type];
 			}
-			
+
 			// This is used way down in the "visible on x other maps" loop
 			var currentMap = dataService.currentMap();
-			
+
 			// If the layer is not visible, create it
 			polyState[type] = [];
 			enabledLayers.push(type);
@@ -215,7 +216,7 @@ define(['lodash'], function(_) {
 				// Detect whether this layer was disabled since this function
 				// was called. This should rarely happen because callbacks are canceled.
 				if (!polyState[type]) return;
-				
+
 				if (feature.properties.type == type && feature.geometry) {
 					var points = dataService.geoJSONToLeaflet(feature.geometry.coordinates[0]);
 					var newPoly = L.polygon(points, {color: color, weight: 2});
@@ -226,9 +227,9 @@ define(['lodash'], function(_) {
 						if (feature.properties.link) {
 							content = '<a href="' + feature.properties.link + '" target="blank_">' + content + '</a>';
 						}
-						var numMaps = feature.properties.maps.length - 1; // Subtract one for the current map
-						if (numMaps > 0) {
-							content += '<details><summary>This feature appears on '+numMaps+' other '+(numMaps === 1 ? 'map' : 'maps')+'</summary>';
+						//var numMaps = feature.properties.maps.length - 1; // Subtract one for the current map
+						//if (numMaps > 0) {
+							/*content += '<div><summary>This feature appears on '+numMaps+' other '+(numMaps === 1 ? 'map' : 'maps')+'</summary>';
 							Object.keys(feature.properties.maps).map(function(k) {
 								return feature.properties.maps[k];
 							}).sort(function(a, b) {
@@ -238,10 +239,24 @@ define(['lodash'], function(_) {
 								if (mapId === currentMap) return;
 								content += '<a class="show_other_map" data-map-id="'+mapId+'">'+mapManager.mapLabel(mapId)+'</a>';
 							});
-							content += '</details>';
-						} else {
-							content += '<br />';
-						}
+							content += '</div>';*/
+							var maps = mapManager.getMaps();
+							console.log("Mappe" , maps);
+							content += '<div>';
+						//	console.log("Mappe", mapManager, maps, feature.properties.maps);
+							Object.keys(maps).map(function(k) {
+								// content += '<p map_id="'+k+'">' + maps[k].name + '</p>';
+								if (k === currentMap) return;
+								if (feature.properties.maps.indexOf(k) >= 0) {
+									content += '<a class="show_other_map" data-map-id="'+k+'">'+mapManager.mapLabel(k)+'</a>';
+								} else {
+									content += '<p class="map_not_linked">'+mapManager.mapLabel(k)+'</p>';
+								}
+							});
+							content += '</div>';
+						//} else {
+						//	content += '<br />';
+						//}
 						if (dataService.auth.getUser()) {
 							content += '<a class="edit" href="#">Edit</a> <a class="clone" href="#">Clone</a> <a class="delete" href="#">Delete</a>';
 						}
@@ -260,62 +275,62 @@ define(['lodash'], function(_) {
 					newPoly.on('dblclick', function() {
 						mapManager.map.setView(feature.properties.center).setZoom(8);
 					});
-				
+
 					newPoly.featureId = feature.id;
 					newPoly.addTo(mapManager.map);
 					polyState[type].push(newPoly);
-				
+
 					activeLandmarksObj[feature.id] = feature;
 				}
 			});
 
 			$('#'+type+'-layer').css('font-weight', 600);
 		};
-		
+
 		this.disableLayer = function(type) {
 			// Cancel pending requests for this layer's geometry
 			dataService.cancelGeometryRequests(type);
-			
+
 			// If the layer is visible, remove it
 			for (var i = 0; i < polyState[type].length; ++i) {
 				mapManager.map.removeLayer(polyState[type][i]);
 				delete activeLandmarksObj[ polyState[type][i].featureId ];
 			}
-			
+
 			enabledLayers.splice(enabledLayers.indexOf(type), 1);
-			
+
 			$('#'+type+'-layer').css('font-weight', 400);
 
 			delete polyState[type];
 		};
 		var myEnableLayer  = self.enableLayer;
 		var myDisableLayer = self.disableLayer;
-		
+
 		this.reload = function(map, selectedFeatureId) {
 			dataService.cancelGeometryRequests();
 			Object.keys(polyState).forEach(function(layerName) {
-			  
+
 				self.disableLayer(layerName);
 				self.enableLayer(layerName, undefined, selectedFeatureId);
 			});
 		};
-		
+
 		this.updateAutocomplete = function() {
 			// Update autocomplete based on selected layers
-			var activeLandmarks; 
+			var activeLandmarks;
 			if (Object.keys(polyState).length === 0) {
 				activeLandmarks = dataService.all();
 			} else {
 				activeLandmarks = Object.keys(activeLandmarksObj).map(function(k){ return activeLandmarksObj[k]; });
 			}
-	
-			$('.search').autocomplete({ 
+
+			$('.search').autocomplete({
 				source: activeLandmarks.map(function(feature) {
 					return feature.properties.name;
 				})
 			});
 		};
 	}
-	
+
 	return LayerManager;
 });
--- js/MapManager.js
+++ js/MapManager.js
@@ -1,29 +1,33 @@
 define(['jquery', 'Leaflet', 'LeafletMiniMap'], function($, L) {
-	
+
 	function MapManager(dataService) {
 		var self = this;
 		var currentMapGeometries = {};
 		var maps = {};
 		var switchFunc = function() {};
-		
+
 		var mainLayer, miniMap;
-		
+
 		function shiftBounds (bounds) {
 			return [[bounds[0][0], bounds[0][1] + 360], [bounds[1][0], bounds[1][1] + 360]];
 		}
-		
+
 		this.mapLabel = function (mapId) {
 			return '<b>'+maps[mapId].year+'</b>: '+maps[mapId].name;
 		};
-		
+
 		this.getMap = function (mapId) {
 			return maps[mapId];
 		};
-		
+
+		this.getMaps = function(){
+			return maps;
+		};
+
 		this.switchMap = function(newMapId, selectedFeatureId) {
 		  console.log("switch to " + newMapId);
 			dataService.removeGeometries();
-			
+
 			dataService.fb.child('maps').child(newMapId).once('value', function(mapSnap) {
 				var mapData = mapSnap.val();
 				if (!mapData) {
@@ -31,54 +35,54 @@ define(['jquery', 'Leaflet', 'LeafletMiniMap'], function($, L) {
 				  return ;
 				}
 				var tileUrl = mapData.tiles;
-				
+
 				if (mainLayer) self.map.removeLayer(mainLayer);
 				mainLayer = L.tileLayer(tileUrl+'/{z}/{x}/{y}.png', {
-					minZoom: 2, 
-					maxZoom: newMapId === 'debarbari' ? 8 : 7, //! HACK -- deBarbari has more zoom levels than the rest 
-					tms: true, 
-					bounds: mapData.bounds, 
+					minZoom: 2,
+					maxZoom: newMapId === 'debarbari' ? 8 : 7, //! HACK -- deBarbari has more zoom levels than the rest
+					tms: true,
+					bounds: mapData.bounds,
 					contonuousWorld: true
 				}).addTo(self.map);
-				
+
 				if (miniMap) self.map.removeControl(miniMap);
 				var tms2 = L.tileLayer(tileUrl+'/{z}/{x}/{y}.png', {
-					minZoom: 1, 
-					maxZoom: 1, 
+					minZoom: 1,
+					maxZoom: 1,
 					tms: true
 				});
 				miniMap = new L.Control.MiniMap(tms2, { toggleDisplay: true }).addTo(self.map);
-				
+
 				if (mapData.bounds) self.map.fitBounds(shiftBounds(mapData.bounds));
-				
+
 				dataService.setMap(newMapId);
 				switchFunc(mapSnap.val(), selectedFeatureId);
-				
+
 				$('.map-menu-link').css('font-weight', 400);
 				$('#'+newMapId+'-map').css('font-weight', 600);
 			});
 		};
-		
+
 		this.onSwitch = function (func) {
 			switchFunc = func;
 		};
-		
+
 		/* Create a menu item for each map
 		 */
 		this.initMenu = function() {
 			dataService.fb.child('maps').orderByChild('year').on('child_added', function (snapshot) {
 				var data = snapshot.val();
 				maps[snapshot.key()] = data;
-					
+
 				$(document).ready(function() {
 					var newOption = '<li role="presentation"><a role="menuitem"'+(data.id === dataService.currentMap()  ? ' style="font-weight: 600;"' : '')+' class="map-menu-link" id="'+data.id+'-map" href="#">'+self.mapLabel(data.id)+'</a></li>';
-			
+
 					if (data.parent) {
 						var strippedParentName = data.parent.replace(/\s/g, '');
 						if ($('#'+strippedParentName+'-menu').length === 0) {
 							var newGroup = ' <li class="dropdown-submenu"><a href="#">'+data.parent+'</a><ul id="'+strippedParentName+'-menu" class="dropdown-menu"></ul></li>';
 							$('#new-map-parent-other').before('<option value="'+data.parent+'">'+data.parent+'</option>');
-			
+
 							if ($('#new-map-menu').length > 0) {
 								$('#new-map-menu').before(newGroup);
 							} else {
@@ -93,20 +97,20 @@ define(['jquery', 'Leaflet', 'LeafletMiniMap'], function($, L) {
 							$('.maps-menu').append(newOption);
 						}
 					}
-					
+
 					$('.maps-menu').removeClass('loading');
-			
+
 					$('#' + data.id + '-map').click(self.switchMap.bind(this, data.id));
 					$('.maps-select').append('<option value="'+data.id+'">'+data.name+'</option>');
 				});
 			});
 		};
-		
+
 		this.initMap = function() {
 			// Initialize leaflet map
 			self.map = L.map('map', { center: [-73, 294], zoom: 3, attributionControl: false });
 			window.lmap = self.map;
-			
+
 			self.switchMap( dataService.currentMap() );
 
 			// Add zoom out button
@@ -139,7 +143,7 @@ define(['jquery', 'Leaflet', 'LeafletMiniMap'], function($, L) {
 			this.map.addControl(new ViewAllControl());
 
 		};
-		
+
 		/* Add a new layer to the map.
 		 * Updates the database and the layer menu
 		 */
@@ -159,15 +163,15 @@ define(['jquery', 'Leaflet', 'LeafletMiniMap'], function($, L) {
 			}
 
 			dataService.fb.child('maps').child(id).set({
-				name: name, 
-				id: id, 
-				year: year, 
-				tiles: tiles, 
+				name: name,
+				id: id,
+				year: year,
+				tiles: tiles,
 				createdBy: dataService.auth.getUser().uid
 			});
 			$('#new-map').modal('hide');
 		};
 	}
-	
+
 	return MapManager;
 });
