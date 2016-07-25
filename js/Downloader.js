b84b1af6b3704aaee65c6fc1dffab268bc9fe3f9
Mattia Fusaro <mattiafusaro8@gmail.com>
2016-07-25 17:34:53 +0200

    aggiornamento libbrerie

--- js/Downloader.js
+++ js/Downloader.js
@@ -1,5 +1,5 @@
 define(function() {
-	
+
 	/* Holds functions related to downloading images from the map
 	 */
 	function Downloader () {
@@ -10,40 +10,41 @@ define(function() {
 			c.width = $("#map").width();
 			c.height = $("#map").height();
 			var canvas = c.getContext("2d");
-	
+
 			var imgs = $(".leaflet-tile-container.leaflet-zoom-animated").children();
-	
+
 			for (var i = 0; i < imgs.length; ++i) {
 				var img = imgs[i];
+				img.crossOrigin = 'anonymous'; //TODO nel caso di localhost scatta la cross domain policy
 				var rect = img.getBoundingClientRect();
-	
+
 				canvas.drawImage(img, rect.left, rect.top);
 			}
-	
+    
 			return c;
 		}
-	
+
 		/* Returns an image URL from data on the map
 		 */
 		this.getData = function () {
 			var c = getCanvasFromMap();
-	
+
 			var img_url = c.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 			return img_url;
-	
+
 		};
-	
+
 		/* Downloads a portion of the map specified by the arguments
 		 */
 		this.downloadSection = function (x, y, width, height) {
 			var canvas = getCanvasFromMap();
 			var ctx = canvas.getContext('2d');
-	
+
 			var selection = ctx.getImageData(x, y, width, height);
 			canvas.width = width;
 			canvas.height = height;
 			ctx.putImageData(selection, 0, 0);
-	
+
 			var link = document.createElement("a");
 			link.href = canvas.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 			link.download = "explorer.png";
@@ -51,8 +52,8 @@ define(function() {
 			theEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 			link.dispatchEvent(theEvent);
 		};
-	
+
 	}
-	
+
 	return Downloader;
 });
